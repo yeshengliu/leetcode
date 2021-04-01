4304f4afa5dc0305b3801bc59f544e8db08573f1
Yesheng Liu <yesheng.liu@outlook.com>
2021-04-01 02:38:31 -0700

    update

---
+++ 286-WallsandGates.java
@@ -0,0 +1,43 @@
+class Solution {
+    
+    // Initiate BFS from gates
+    private static final int EMPTY = Integer.MAX_VALUE;
+    private static final int GATE = 0;
+    private static final List<int[]> DIRECTIONS = Arrays.asList(
+        new int[] {1, 0},
+        new int[] {-1, 0},
+        new int[] {0, 1},
+        new int[] {0, -1}
+    );
+    
+    public void wallsAndGates(int[][] rooms) {
+        int m = rooms.length;
+        if (m == 0) return;
+        int n = rooms[0].length;
+        Queue<int[]> q = new LinkedList<>();
+        // Find all gates and push them in the queue
+        for (int row = 0; row < m; row++) {
+            for (int col = 0; col < n; col++) {
+                if (rooms[row][col] == GATE) {
+                    q.add(new int[] { row, col });
+                }
+            }
+        }
+        // Begin BFS
+        while (!q.isEmpty()) {
+            int[] point = q.poll();
+            int row = point[0];
+            int col = point[1];
+            for (int[] direction: DIRECTIONS) {
+                int r = row + direction[0];
+                int c = col + direction[1];
+                // r, c is invalid index, or rooms[r][c] is not empty room or already visited
+                if (r < 0 || c < 0 || r >= m || c >= n || rooms[r][c] != EMPTY){
+                    continue;
+                }
+                rooms[r][c] = rooms[row][col] + 1;
+                q.add(new int[] { r, c });
+            }
+        }
+    }
+}
